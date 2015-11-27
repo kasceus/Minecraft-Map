db81e99bd0fd18bc5b9bc07a12f1b333e913fdb1
kasceus <chaz92487@gmail.com>
2015-11-28 01:39:21 +0900

    added all files to the map

---
+++ config.js
@@ -0,0 +1,50 @@
+var CONFIG = {
+  "maps": {
+    "map_myworld": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1448637358,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 10,
+      "name": "My World",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "myworld_isometric_t1",
+      "tileSize": 384,
+      "world": "myworld",
+      "worldName": "myworld"
+    }
+  },
+  "mapsOrder": [
+    "map_myworld"
+  ],
+  "tileSetGroups": {
+    "myworld_isometric_t1": {
+      "maxZoom": 10,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ]
+    }
+  }
+};
